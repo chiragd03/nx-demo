cd5c8f96fac027a3d152a1d4402e05bd544313d1
Chirag Dahiya <cdah@softchoice.com>
2023-06-01 17:01:30 -0400

    build: add commment

--- packages/is-odd/index.ts
+++ packages/is-odd/index.ts
@@ -1,3 +1,6 @@
 import { isEven } from 'is-even';
 
-export const isOdd = (x: number)  =>     !isEven(x);
+export const isOdd = (x: number)  => {
+    console.log('comment')
+    return !isEven(x);
+}     
