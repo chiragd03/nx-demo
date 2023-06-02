b10963f149afca40387d034b4e973edb2a81cef7
Chirag Dahiya <cdah@softchoice.com>
2023-06-02 09:52:14 -0400

    build: changed code again

--- packages/is-odd/index.ts
+++ packages/is-odd/index.ts
@@ -1,6 +1,6 @@
 import { isEven } from 'is-even';
 
 export const isOdd = (x: number)  => {
-    console.log('comment updated')
+    console.log('comment updated once more')
     return !isEven(x);
 }     
